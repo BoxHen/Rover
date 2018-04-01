f2f4dcb9991a6198d42564de74298d5bee7959cd
Henry <hchen101@binghamton.edu>
2018-03-31 20:07:53 -0400

    integrated PWM

--- src/test/main/a_Ultrasonic_Test/a_Ultrasonic_Test.ino
+++ src/test/main/a_Ultrasonic_Test/a_Ultrasonic_Test.ino
@@ -2,7 +2,7 @@
 
 //#include <EEPROM.h>
 
-/*===============================DEFINITIONS FOR ULTRASONIC SENSOR==================================*/
+/*========================DEFINITIONS/VARIABLES FOR ULTRASONIC SENSOR================================*/
 #define trigPin1 3
 #define echoPin1 2 // Right
 
@@ -12,9 +12,17 @@
 //#define trigPin3 7
 //#define echoPin3 8
 
-/*===============================VARIABLES FOR ULTRASONIC SENSOR====================================*/
 long duration, distance, RightSensor,FrontSensor,LeftSensor;
 
 /*=====================================VARIABLES FOR MOTOR==========================================*/
 int mLeft = 9; //left
 int mRight =10; //right
+/*================================DEFINITIONS/VARIABLES FOR PWM=====================================*/
+// This is the PWMthat goes to motor 
+#define PWMout 11 // LED connected to digital pin 9
+
+//int analogPin = 12; // Use this if we decide for variable value - slow down/up motor speed
+int val = 400; // variable to store the read value
+// this is a little over 1/3 speed where 1023 is max speed
+
+
--- src/test/main/a_Ultrasonic_Test/c_LOOP.ino
+++ src/test/main/a_Ultrasonic_Test/c_LOOP.ino
@@ -34,4 +34,18 @@ void loop() {
   }
   // will need a reverse!!!
 
+  /*================================================PWM=======================================================*/
+  //val = analogRead(analogPin); // Use if we want to speed/slow motor dynamically
+                                 // analogRead will get a value from 0-1023
+  analogWrite(ledPin, (val / 4)); // value can be set to constant for specific speed or connected to some input to control motor speed
+   /*
+      analogWrite(pin, value)
+      pin: the pin to write to. Allowed data types: int.
+      value: the duty cycle: between 0 (always off) and 255 (always on). Allowed data types: int
+
+      analogRead values go from 0 to 1023,
+      analogWrite values from 0 to 255
+
+      reference: https://www.tutorialspoint.com/arduino/arduino_pulse_width_modulation.htm */
+
 }
