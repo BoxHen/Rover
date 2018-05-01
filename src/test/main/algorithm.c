83cabe27c1cbe365ca90d9135e3941826fa23e92
Henry <hchen101@binghamton.edu>
2018-04-30 21:18:40 -0400

    update algorithm

--- src/test/main/algorithm.c
+++ src/test/main/algorithm.c
@@ -9,7 +9,7 @@
 void algorithm(){
   init_motors();
   init_ultrasonic();
-  int decision, prev_decision;
+  int decision, prevDecision, turnCount = 0;
 
   if(distanceL < distanceR) {decision = 1;}
   else {decision = 2;}
@@ -21,22 +21,18 @@ void algorithm(){
 
     switch(decision){
       case 1:// left wall is closer
-          prev_decision = decision;
           turnLeft(); _delay_ms(1050);
-          while( distanceF >= 10 ){
-            forward(); _delay_ms(100); // used so motors move or else it loops to fast for motors to process
-          }
-          if(distanceUF >= 10){decision = 3;} // obstacle avoid case
-          else {decision = *********;} // corner turn case
+          prevDecision = 1; // used to alternate between turns for going up and turn maze
+          turnCount++;
+          if(turnCount >= 3){decision = 6} // this will execute the updown traversal after we get to top left corner
+          else {decision = 5;}
           break;
       case 2: // right wall is closer
-          prev_decision = decision;
           turnRight(); _delay_ms(1050);
-          while( distanceF >= 10 ){
-            forward(); _delay_ms(100); // used so motors move or else it loops to fast for motors to process
-          }
-          if(distanceUF >= 10){decision = 4;} // obstacle avoid case
-          else {decision = *********;};} // corner turn case
+          prevDecision = 2; // used to alternate between turns for going up and turn maze
+          turnCount++;
+          if(turnCount >= 3){decision = 7} // this will execute the updown traversal after we get to top right corner
+          else {decision = 5;}
           break;
       case 3: // obstacle turn for left wall start
           turnRight(); _delay_ms(1050);
@@ -47,9 +43,9 @@ void algorithm(){
           turnLeft(); _delay_ms(1050);
           while(distanceF >= 10){ forward(); } // move forward until front sensor sees a wall
           turnRight(); _delay_ms(1050);
-          decision = *********;};
+          decision = 5;};
           break;
-      case 4:
+      case 4: // obstacle turn right
           turnLeft(); _delay_ms(1050);
           while(distanceL <= 15){ forward(); } // move forward if you are against the wall of the obstacle
           turnRight(); _delay_ms(1050);
@@ -58,8 +54,29 @@ void algorithm(){
           turnRight(); _delay_ms(1050);
           while(distanceF >= 10){ forward(); } // move forward until front sensor sees a wall
           turnLeft(); _delay_ms(1050);
-          decision = *********;};
+          decision = 5;};
           break;
+      case 5: // move forward until we see a obstacle or wall
+          while( distanceF >= 10 ){
+            forward(); _delay_ms(100); // used so motors move or else it loops to fast for motors to process
+          }
+          if     ( distanceUF >= 10 && distanceL <= 10 ){decision = 3; break;} // obstacle avoid case left
+          else if( distanceUF >= 10 && distanceR <= 10 ){decision = 4; break;} // obstacle avoid right
+          else if( distanceF <= 10 && distanceL <= 10 ){decision = 2;  break;} // corner turn case for left
+          else if( distanceF <= 10 && distanceR <= 10 ){decision = 1;  break;} //corner turn right
+          else if( prevDecision == 1 ) {decision = 2; break;}
+          else decision = 1; break;
+      case 6: // up/dowm motion from left corner
+          //prevDecision = 6; // used to alternate between turns for going up and turn maze
+          forward(); _delay_ms(400); // test this delay
+          turnRight(); _delay_ms(1050);
+          decision = 5;
+      case 7: // up/down motion from right corner
+          //prevDecision = 7; // used to alternate between turns for going up and turn maze
+          forward(); _delay_ms(400); // test this delay
+          turnLeft(); _delay_ms(1050);
+          decision = 5;
+
 
     }
 
